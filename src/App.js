cbb6de5b6c38bd51d67501c387cc6593602bc6d6
Dariy98 <ostapenko.d98@gmail.com>
2020-07-23 23:55:27 +0300

    corected errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-import Header from './components/Header'
-import Post from './components/Post'
-import Comments from './components/Comments'
-import Form from './components/Form'
-
+import Header from './components/Header';
+import Post from './components/Post';
+import Comments from './components/Comments';
+import Form from './components/Form';
 
 function App() {
 
@@ -13,22 +11,17 @@ function App() {
   const [name, setName] = useState("");
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
+    fetch("https://jsonplaceholder.typicode.com/posts/1?_embed=comments&_expand=user")
       .then(response => response.json())
-      .then(data => setPost(data))
+      .then(data => getData(data.comments, data.title, data.body, data.user.name))
   }, []);
 
-  useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/posts/11?_embed=comments")
-        .then(response => response.json())
-        .then(data => setComments(data.comments))
-  }, []);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1?_expand=user")
-      .then(response => response.json())
-      .then(response => setName(response.user.name))
-  }, []);
+  const getData = (c, title, body, author) => {
+    let postData = { title, body}
+    setComments(c)
+    setPost(postData)
+    setName(author)
+  }
 
   return (
     <div className="App">
@@ -38,7 +31,7 @@ function App() {
             <div className="uk-container">
                 <Post post={post} name={name}/>
                 <Comments comments={comments}/>
-                <Form comments={comments}/>
+                <Form comments={comments} setComments={setComments}/>
             </div>
         </div>
       </main>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-export default function Form ({comments}) {
+export default function Form ({comments, setComments}) {
 
     const [name, setNewName] = useState("");
     const [email, setEmail] = useState("");
     const [comment, setComment] = useState("");
+    const [nameValue, setValueName] = useState("")
+    const [emailValue, setValueEmail] = useState("")
+    const [commentValue, setValueComment] = useState("")
 
-    const getName = (event) =>  setNewName(event.target.value);
+    const getName = (event) =>  {
+        setValueName(event.target.value)
+        setNewName(event.target.value)
+    };
 
-    const getEmail = (event) => setEmail(event.target.value);
+    const getEmail = (event) => {
+        setEmail(event.target.value)
+        setValueEmail(event.target.value)
+    };
 
-    const getComment = (event) => setComment(event.target.value);
+    const getComment = (event) => {
+        setComment(event.target.value)
+        setValueComment(event.target.value)
+    };
 
     const generatorID = () => {return Date.now()};
 
-    const getAllCommentInfo = () => {
-        let commentInfo = {
-            postId: 11,
-            id: generatorID(),
-            name,
-            email,
-            comment
-        }
-        return commentInfo;
+    let commentInfo = {
+        postId: 11,
+        id: generatorID(),
+        name,
+        email,
+        body: comment
     }
 
     const postComment = (e) => {
@@ -32,11 +41,19 @@ export default function Form ({comments}) {
             headers: {
               "Content-Type": "application/json"
             },
-            body: JSON.stringify(getAllCommentInfo())
-        }).then(comments.push(getAllCommentInfo()))
+            body: JSON.stringify(commentInfo)
+        })
+        
+        upDateComments()
+        setValueName("")
+        setValueEmail("")
+        setValueComment("")
+    }
 
-        console.log(comments)
-        return comments
+    const upDateComments = () => {
+        let arr = comments
+        arr.push(commentInfo)
+        setComments(arr => [...arr])
     }
 
     return(
@@ -44,16 +61,16 @@ export default function Form ({comments}) {
             <fieldset className="uk-fieldset">
                 <legend className="uk-legend">Add Comment</legend>
                 <div className="uk-margin">
-                    <input className="uk-input" type="text" placeholder="Name" required onChange={getName}/>
+                    <input className="uk-input" type="text" value={nameValue} placeholder="Name" required onChange={getName}/>
                 </div>
                 <div className="uk-margin">
-                    <input className="uk-input" type="email" placeholder="Email" required onChange={getEmail}/>
+                    <input className="uk-input" type="email" value={emailValue} placeholder="Email" required onChange={getEmail}/>
                 </div>
                 <div className="uk-margin">
-                    <textarea className="uk-textarea" rows="5" placeholder="Comment" required onChange={getComment}></textarea>
+                    <textarea className="uk-textarea" rows="5" value={commentValue} placeholder="Comment" required onChange={getComment}></textarea>
                 </div>
                 <div className="uk-margin">
-                    <button className="uk-button uk-button-primary" type="submit" onClick={(e) => postComment(e)}>Post Comment</button>
+                    <button className="uk-button uk-button-primary" type="submit" onClick={((e) => postComment(e))}>Post Comment</button>
                 </div>
             </fieldset>
         </form>
